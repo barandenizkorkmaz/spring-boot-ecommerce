a16cb2ad4f2ba15f07b2823e957d254b096b97df
Baran Deniz Korkmaz <barandenizkorkmaz@gmail.com>
2025-01-20 15:28:06 +0100

    s9: add dependency lombok

--- src/main/java/com/org/ecommerce/model/Category.java
+++ src/main/java/com/org/ecommerce/model/Category.java
@@ -4,28 +4,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
